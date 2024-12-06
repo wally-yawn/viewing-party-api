e162a1e788b4e885ba022274910bc2f353ff9908
Wally Wallace <jrvw001@gmail.com>
2024-12-06 10:44:58 -0700

    Add additional test for movie gateway method

--- spec/gateways/movie_gateway_spec.rb
+++ spec/gateways/movie_gateway_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe MovieGateway do
     expect(movies.length).to eq(20)
     expect(movies[0].title).to eq("Moana 2")
   end
+
+  it 'gets movies based on a search', :vcr do
+    movies = MovieGateway.search_movies("bond")
+    expect(movies.length).to eq(20)
+    expect(movies[0].title).to eq("Demon Slayer: Kimetsu no Yaiba – Sibling's Bond")
+  end
 end
